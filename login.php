d092d5a5c9ae8e65991333d1d52413448bcca94d
unknown <mo3taz@gmail.com>
2022-05-10 11:54:23 +0300

    added Login page

---
+++ login.php
@@ -0,0 +1,8 @@
+<<?php
+$ogrencinumara = moataz.armash@ogr.sakarya.edu.tr;
+$sifre = g201210584;
+if ($ogrencinumara == kullancı && $sifre == şifre)
+echo "Hoş Geldiniz". $ogrencinumara;
+else 
+echo "Lütfen Tekrar deneyiniz";
+?>
