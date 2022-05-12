5a0f4a77dd0b0eadf1101c06e6af5193e2bfee5e
unknown <mo3taz@gmail.com>
2022-05-12 15:59:39 +0300

    added php file to login page

--- login.php
+++ login.php
@@ -1,8 +1,22 @@
-<<?php
-$ogrencinumara = moataz.armash@ogr.sakarya.edu.tr;
-$sifre = g201210584;
-if ($ogrencinumara == kullancı && $sifre == şifre)
-echo "Hoş Geldiniz". $ogrencinumara;
-else 
-echo "Lütfen Tekrar deneyiniz";
+<?php 
+$kullancıAdı = "g201210584@sakarya.edu.tr";
+// $kullancıErr = $emailErr = "";
+// $email  = "";
+
+  
+
+
+
+    if (($_POST["kullancı"]) == ($kullancıAdı) && ($_POST["şifre"]) == ("g201210584"))
+    {
+       echo "HOŞ GELDİNİZ ". substr($kullancıAdı,0,10) ; 
+    }
+    else 
+    {
+        echo ("<script>
+    window.alert('Girmiş olduğunuz kullancı adı ya da şifreyi Yanlıştır Lütfen tekrar deneyin');
+    window.location.href='login.html';
+    </script>");
+    }
+
 ?>
